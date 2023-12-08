80d59f7abf7ef0238661642a2ba83203a511ef74
Ofir Katz <ofirkatz94@gmail.com>
2023-12-08 21:17:10 +0100

    feat: using material's table schematic

---
+++ src/app/commerces-table/commerces-table.component.spec.ts
@@ -0,0 +1,34 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+import { CommercesTableComponent } from './commerces-table.component';
+
+describe('CommercesTableComponent', () => {
+  let component: CommercesTableComponent;
+  let fixture: ComponentFixture<CommercesTableComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [CommercesTableComponent],
+      imports: [
+        NoopAnimationsModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatTableModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CommercesTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
